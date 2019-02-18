276e2e239930250893bda8533b9438d8938398fa
will1001 <wilirahmatm@gmail.com>
2019-02-18 13:42:31 +0800

    first commit

---
+++ resources/views/adminkadus.blade.php
@@ -0,0 +1,381 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="John Doe">
+    <meta name="description" content="">
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Title -->
+    <title>Desa Jenggik Utara</title>
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="apple-touch-icon" href="{{asset('images/apple-touch-icon.png')}}">
+    <link rel="shortcut icon" type="image/ico" href="{{asset('images/favicon.ico')}}" />
+    <!-- Plugin-CSS -->
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('css/animate.css')}}">
+    <!-- Main-Stylesheets -->
+    <link rel="stylesheet" href="{{asset('css/normalize.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/responsiveadmin.css')}}">
+    <link rel="stylesheet" href="{{asset('css/admin.css')}}">
+    <link rel="stylesheet" href="{{asset('css/table.css')}}">
+    <script src="{{asset('js/vendor/modernizr-2.8.3.min.js')}}"></script>
+
+    <!--[if lt IE 9]>
+        <script src="{{asset('')}}//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="{{asset('')}}//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body data-spy="scroll" data-target="#primary-menu">
+
+
+
+    <!--Mainmenu-area-->
+    <div class="mainmenu-area" data-spy="affix" data-offset-top="100">
+        <div class="container">
+            <!--Logo-->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#primary-menu">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a href="{{url('/')}}" class="navbar-brand logo">
+                  <img id="logo" src="images/kabupaten-lombok-timur-ntb (1).png" alt="">
+                    <h5><strong>Website Resmi Pemerintah</strong></h5>
+                    <h2>Desa Jenggik Utara</h2>
+                </a>
+            </div>
+            <!--Logo/-->
+            <nav class="collapse navbar-collapse" id="primary-menu">
+                <ul class="nav navbar-nav navbar-right">
+                    @guest
+                          <li>
+                              <a href="{{ route('login') }}">{{ __('Login') }}</a>
+                          </li>
+                    @else
+                          
+                          <li>
+                               <a class="dropdown-item" href="{{ route('logout') }}"
+                                                     onclick="event.preventDefault();
+                                                                   document.getElementById('logout-form').submit();">
+                                      {{ __('Logout') }}
+                                  </a>
+
+                                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                      @csrf
+                                  </form>
+                          </li>
+                      @endguest
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <!--Mainmenu-area/-->
+
+
+
+
+
+
+
+<br><br><br><br><br>
+<h1 class="text-center">Admin Dusun {{$kode_area_dusuns[0]->Nama_Dusun}}</h1>
+
+    
+
+    <section id="tabeldatapendudukkadus"  class="section-padding">
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-12 col -sm-12 col-md-6">
+              <select id="filterkadus">
+                 <option selected="true" disabled="disabled">Cari Berdasarkan</option>
+                  <option value="Nama">Nama</option>
+                  <option value="NIK">NIK</option>
+                  <option value="Nomor_kk">Nomor KK</option>
+                  <option value="Pendidikan">Pendidikan</option>
+                  <option value="Status_Perkawinan">status Perkawinan</option>
+                  <option value="Golongan_Darah">Golongan Darah</option>
+            </select>
+             <div class="searchkadus">
+            <input type="text"  id="searchkadus" name="searchkadus" placeholder=". . ." ></input>
+            </div>
+           <button id="tombol_searchkadus">Search</button>  
+           
+            
+            </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <h1 style="margin-bottom: 35px;">Tabel Data Penduduk Dusun {{$kode_area_dusuns[0]->Nama_Dusun}}</h1>
+
+           <div style="overflow: auto;max-height: 400px;position: relative;  ">
+      <table id="tabeldatakadus">
+      <thead>
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <col width="1000px">
+        <tr>
+                  <th rowspan="2">No</th>
+                  <th rowspan="2">Alamat</th>
+                  <th rowspan="2">RW</th>
+                  <th rowspan="2">RT</th>
+                  <th rowspan="2">Nama</th>
+                  <th rowspan="2">Nomor KK</th>
+                  <th rowspan="2">Nomor NIK</th>
+                  <th rowspan="2">Jenis Kelamin</th>
+                  <th rowspan="2">Tempat Lahir</th>
+                  <th rowspan="2">Tanggal Lahir</th>
+                  <th rowspan="2">Usia</th>
+                  <th rowspan="2">Agama</th>
+                  <th rowspan="2">Pendidikan</th>
+                  <th rowspan="2">Jenis Pekerjaan</th> 
+                  <th rowspan="2">Status Perkawinan</th> 
+                  <th rowspan="2">Status Hubungan Dalam Keluarga</th> 
+                  <th rowspan="2">Kewarganegaraan</th> 
+                  <th rowspan="2">Nama Ayah</th> 
+                  <th rowspan="2">Nama Ibu</th> 
+                  <th rowspan="2">Golongan Darah</th> 
+                  <th rowspan="2">Akta Lahir</th> 
+                  <th rowspan="2">Nomor Dokumen Paspor</th>
+                  <th rowspan="2">Tanggal Akhir Paspor</th>  
+                  <th rowspan="2">Nomor Dokumen KITAS</th>             
+                  <th rowspan="2">NIK Ayah</th> 
+                  <th rowspan="2">NIK Ibu</th> 
+                  <th rowspan="2">No Akta Perkawinan</th> 
+                  <th rowspan="2">Tanggal Perkawinan</th> 
+                  <th rowspan="2">No Akta Perceraian</th> 
+                  <th rowspan="2">Tanggal Perceraian</th> 
+                  <th rowspan="2">Cacat</th> 
+                  <th rowspan="2">Cara KB</th> 
+                  <th rowspan="2">Hamil</th> 
+                  <th rowspan="2">Status kependudukan</th> 
+                  <th rowspan="2">Keterangan</th>  
+                  <th rowspan="2">Tempat Mendapatkan Air Bersih</th>  
+                  <th rowspan="2">Status Gizi Balita</th>  
+                  <th rowspan="2">Kebiasaan Berobat Bila Sakit</th>   
+                  <!-- <th colspan="5">Pendapatan Perkapita</th>    -->
+                  <!-- <th colspan="5">Pendapatan Rill Keluarga</th>    -->
+                  <th rowspan="2">Foto KTP</th>  
+                  <th rowspan="2">Foto KK</th>   
+                  <th rowspan="2">edit</th>  
+        </tr>
+      </thead>
+      <tbody id="tbodytabel">
+        @php
+        $no=1
+        @endphp
+        @foreach($data_penduduks as $data_penduduk)
+          <tr>
+            <td>{{ $no++ }}</td>
+            <td>{{ $data_penduduk->Alamat }}</td>
+            <td>{{ $data_penduduk->RW }}</td>
+            <td>{{ $data_penduduk->RT }}</td>
+            <td>{{ $data_penduduk->Nama }}</td>
+            <td>{{ $data_penduduk->Nomor_KK }}</td>
+            <td>{{ $data_penduduk->NIK }}</td>
+            <td>{{ $data_penduduk->jenis_kelamin }}</td>
+            <td>{{ $data_penduduk->Tempat_Lahir }}</td>
+            <td>{{ $data_penduduk->Tanggal_Lahir }}</td>
+            <td>{{ $data_penduduk->Usia }}</td>
+            <td>{{ $data_penduduk->agama }}</td>
+            <td>{{ $data_penduduk->pendidikan }}</td>
+            <td>{{ $data_penduduk->jenis_pekerjaan }}</td>
+            <td>{{ $data_penduduk->status_perkawinan }}</td>
+            <td>{{ $data_penduduk->status_hubungan_dalam_keluarga }}</td>
+            <td>{{ $data_penduduk->kewarganegaraan }}</td>
+            <td>{{ $data_penduduk->Nama_Ayah }}</td>
+            <td>{{ $data_penduduk->Nama_Ibu }}</td>
+            <td>{{ $data_penduduk->golongan_darah }}</td>
+            <td>{{ $data_penduduk->Akta_Lahir }}</td>
+            <td>{{ $data_penduduk->No_Paspor }}</td>
+            <td>{{ $data_penduduk->Tanggal_akhir_Paspor }}</td>
+            <td>{{ $data_penduduk->No_KITAS }}</td>
+            <td>{{ $data_penduduk->NIK_Ayah }}</td>
+            <td>{{ $data_penduduk->NIK_Ibu }}</td>
+            <td>{{ $data_penduduk->No_Akta_Perkawinan }}</td>
+            <td>{{ $data_penduduk->Tanggal_Perkawinan }}</td>
+            <td>{{ $data_penduduk->No_Akta_Perceraian }}</td>
+            <td>{{ $data_penduduk->Tanggal_Perceraian }}</td>
+            <td>{{ $data_penduduk->Cacat }}</td>
+            <td>{{ $data_penduduk->Cara_KB }}</td>
+            <td>{{ $data_penduduk->Hamil }}</td>
+            <td>{{ $data_penduduk->Status_kependudukan }}</td>
+            <td>{{ $data_penduduk->Keterangan }}</td>
+            <td>{{ $data_penduduk->tempat_mendapatkan_air_bersih }}</td>
+            <td>{{ $data_penduduk->status_gizi_balita }}</td>
+            <td>{{ $data_penduduk->kebiasaan_berobat_bila_sakit }}</td>
+            @if($data_penduduk->foto_ktp==null && $data_penduduk->foto_kk==null )
+            <td ><a href="{{$data_penduduk->foto_ktp}}"></a></td>        
+            <td ><a href="{{$data_penduduk->foto_kk}}"></a></td>
+            @endif
+            @if($data_penduduk->foto_ktp==null && $data_penduduk->foto_kk!=null)
+            <td ><a href="{{$data_penduduk->foto_ktp}}"></a></td>        
+            <td ><a href="{{$data_penduduk->foto_kk}}">lihat</a></td>
+            @endif
+            @if($data_penduduk->foto_ktp!=null && $data_penduduk->foto_kk==null)
+            <td ><a href="{{$data_penduduk->foto_ktp}}">lihat</a></td>        
+            <td ><a href="{{$data_penduduk->foto_kk}}"></a></td>
+            @endif
+            @if($data_penduduk->foto_ktp!=null && $data_penduduk->foto_kk!=null)
+            <td ><a href="{{$data_penduduk->foto_ktp}}">lihat</a></td>        
+            <td ><a href="{{$data_penduduk->foto_kk}}">lihat</a></td>
+            @endif      
+            <td><a href="formeditdatapendudukkadus/{{ $data_penduduk->NIK }}">edit</a></td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <section id="tabeldatapendudukkadus2"  class="section-padding">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            
+            <input type="text" name="NIK" placeholder="NIK" id="NIK"><br><br>
+            
+
+            <a href="" class="tomboledit" id="edittombol">Penduduk keluar</a>
+            <a href="{{url('formadddatapendudukkadus')}}" class="tomboladd">Penduduk Masuk</a>
+            
+            
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
+   <footer class="footer-area relative sky-bg" id="contact-page">
+        <div class="absolute footer-bg"></div>
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-12">
+                        <address class="side-icon-boxes">
+                            <div class="side-icon-box">
+                                <div class="side-icon">
+                                    <img src="{{asset('images/location-arrow.png')}}" alt="">
+                                </div>
+                                <p><strong>Alamat: </strong>Jl. Jurusan Montong Gading - Jenggik Utara, Km 4, Desa Jenggik Utara.Kode Pos 83664</p>
+                            </div>
+                            <div class="side-icon-box">
+                                <div class="side-icon">
+                                    <img src="{{asset('images/phone-arrow.png')}}" alt="">
+                                </div>
+                                <p><strong>Telpon: </strong>
+                                    08123456789
+                                </p>
+                            </div>
+                            <div class="side-icon-box">
+                                <div class="side-icon">
+                                    <img src="{{asset('images/mail-arrow.png')}}" alt="">
+                                </div>
+                                <p><strong>E-mail: </strong>
+                                    <a href="mailto:youremail@example.com">kantor@desaJenggik Utara.id</a>
+                                    
+                                </p>
+                            </div>
+                        </address>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-middle">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6 pull-right">
+                        <ul class="social-menu text-right x-left">
+                            <li><a href="#"><i class="ti-facebook"></i></a></li>
+                            <li><a href="#"><i class="ti-twitter"></i></a></li>
+                            <li><a href="#"><i class="ti-google"></i></a></li>
+                            <li><a href="#"><i class="ti-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 text-center">
+                        <p>&copy;Copyright 2018.made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://winchy.tech">winchy.tech</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+
+
+    <!--Vendor-JS-->
+    <script src="{{asset('js/vendor/jquery-1.12.4.min.js')}}"></script>
+    <script src="{{asset('js/vendor/bootstrap.min.js')}}"></script>
+    <!--Plugin-JS-->
+    <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('js/contact-form.js')}}"></script>
+    <script src="{{asset('js/jquery.parallax-1.1.3.js')}}"></script>
+    <script src="{{asset('js/scrollUp.min.js')}}"></script>
+    <script src="{{asset('js/magnific-popup.min.js')}}"></script>
+    <script src="{{asset('js/wow.min.js')}}"></script>
+    <!--Main-active-JS-->
+    <script src="{{asset('js/main.js')}}"></script>
+</body>
+
+</html>
