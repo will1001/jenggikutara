a7de8318c08d9844ce38076c829cf1603b76243d
will1001 <wilirahmatm@gmail.com>
2019-02-18 17:10:25 +0800

    perbaikan edit sotk 2

--- resources/views/layouts/layoutform.blade.php
+++ resources/views/layouts/layoutform.blade.php
@@ -23,7 +23,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
   <div class="container">
     <a class="navbar-brand" href="{{url('/')}}">
-    <img src="{{asset('images/logo.png')}}" width="40" height="50" alt="">  Desa Jenggik Utara
+    <img src="{{asset('/images/logo.png')}}" width="40" height="50" alt="">  Desa Jenggik Utara
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
