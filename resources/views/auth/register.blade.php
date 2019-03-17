a85f3c3a98397082e6970a4bac08d6f9356a1d76
will1001 <wilirahmatm@gmail.com>
2019-03-17 11:09:21 +0800

    responsive adminkades 6

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container registerform">
+<div class="container loginregform" id="registerform">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
