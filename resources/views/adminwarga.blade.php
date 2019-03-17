e285e0ac1a9bfcf2b770e4df1d7bb095359cb323
will1001 <wilirahmatm@gmail.com>
2019-03-17 19:58:01 +0800

    fixed adminwarga

--- resources/views/adminwarga.blade.php
+++ resources/views/adminwarga.blade.php
@@ -69,7 +69,7 @@
                           </li>
                           <li class="nav-item dropdown">
                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                  {{ Auth::user()->NIK }} <span class="caret"></span>
+                                  {{ Auth::user()->Nomor_KK }} <span class="caret"></span>
                               </a>
 
                               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
